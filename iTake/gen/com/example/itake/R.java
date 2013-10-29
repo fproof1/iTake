c5da1dce19da4bb3971801dd6170bc0f88004795
lafandahalf <Libby@Libbys-MacBook-Air.local>
2013-10-29 15:53:23 -0400

    test

--- iTake/gen/com/example/itake/R.java
+++ iTake/gen/com/example/itake/R.java
@@ -20,22 +20,43 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class color {
         public static final int black_overlay=0x7f040000;
+        public static final int blue_back=0x7f040002;
+        public static final int grey_button=0x7f040003;
+        public static final int white_back=0x7f040001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
         public static final int dummy_button=0x7f070002;
+        public static final int emaillogin=0x7f070005;
         public static final int fullscreen_content=0x7f070000;
         public static final int fullscreen_content_controls=0x7f070001;
+        public static final int loginbutton=0x7f070008;
+        public static final int loginprompt=0x7f07000a;
+        public static final int newuser_prompt=0x7f070003;
+        public static final int newuserbutton=0x7f07000b;
+        public static final int passwordconfirm=0x7f070007;
+        public static final int passwordlogin=0x7f070006;
+        public static final int reg_prompt=0x7f070004;
+        public static final int welcometv=0x7f070009;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
+        public static final int user_reg=0x7f030001;
+        public static final int welcomsplash=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int dummy_button=0x7f050001;
         public static final int dummy_content=0x7f050002;
+        public static final int email_hint=0x7f050004;
+        public static final int login=0x7f050006;
+        public static final int login_prompt=0x7f050005;
+        public static final int new_user=0x7f050007;
+        public static final int password_confirm_hint=0x7f050009;
+        public static final int password_hint=0x7f050003;
+        public static final int registration=0x7f050008;
     }
     public static final class style {
         /** 
