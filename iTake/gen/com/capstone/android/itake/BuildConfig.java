04eb0975dae73971ceb7036ab708578fbc66c1ea
Wolfpack16 <divinewolf16@hotmail.com>
2013-11-08 09:17:51 -0500

    Alarm Update

--- iTake/gen/com/capstone/android/itake/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.capstone.android.itake;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- iTake/src/com/capstone/android/itake/EditDrug.java
+++
@@ -1,5 +0,0 @@
-package com.capstone.android.itake;
-
-public class EditDrug {
-
-}
